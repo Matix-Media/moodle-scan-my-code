f95ffcece70f3d9ebb47e4139d58761f1161252f
Matix-Media <heilmann@matix-media.net>
2025-07-09 12:41:55 +0200

    fix: :loud_sound: add logging for qr code submit

--- src/http/router.ts
+++ src/http/router.ts
@@ -8,10 +8,15 @@ export default function router(fastify: FastifyInstance, options: FastifyPluginO
         { schema: { body: { type: "object", properties: { key: { type: "string" }, data: { type: "string" } } } } },
         async (req, reply) => {
             const scanToken = fastify.bot().getScanToken(req.body.key);
-            if (!scanToken) return reply.status(401).send({ error: "Invalid key." });
+            if (!scanToken) {
+                req.log.warn("SCAN SUBMIT", `Invalid key: ${req.body.key}`);
+                return reply.status(401).send({ error: "Invalid key." });
+            }
 
-            if (!req.body.data.startsWith(fastify.bot().generateLoginUrl(scanToken.connection)))
+            if (!req.body.data.startsWith(fastify.bot().generateLoginUrl(scanToken.connection))) {
+                req.log.warn("SCAN SUBMIT", `Invalid URL: ${req.body.data}`);
                 return reply.status(400).send({ error: "Invalid url." });
+            }
 
             try {
                 const result = await new Promise<void>((resolve, reject) =>
@@ -25,8 +30,13 @@ export default function router(fastify: FastifyInstance, options: FastifyPluginO
                     ),
                 );
             } catch (err) {
-                if (err instanceof InvalidQRCodeError) return reply.status(400).send({ error: "Invalid qr-code." });
-                else return reply.status(500).send({ error: "Internal server error." });
+                if (err instanceof InvalidQRCodeError) {
+                    req.log.warn("SCAN SUBMIT", `Invalid QR code: ${req.body.data}`);
+                    return reply.status(400).send({ error: "Invalid qr-code." });
+                } else {
+                    req.log.error("SCAN SUBMIT", `Error processing QR code: ${req.body.data}`, err);
+                    return reply.status(500).send({ error: "Internal server error." });
+                }
             }
 
             return reply.status(201).send();
